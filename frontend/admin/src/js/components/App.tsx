e77c61798e4766b71e5bdf472335dc684e37451c
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-09-12 20:59:51 -0400

    Added ProtectedRoute component.

--- frontend/admin/src/js/components/App.tsx
+++ frontend/admin/src/js/components/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import { AuthenticationContext } from "@common/components/Auth";
 import Dashboard from "./dashboard/Dashboard";
 import { useAdminRoutes } from "../adminRoutes";
 import HomePage from "./home/HomePage";
 import DashboardPage from "./dashboard/DashboardPage";
+
+const useAuth = () => {
+  return React.useContext(AuthenticationContext);
+};
+
+const ProtectedRoute = () => {
+  const { loggedIn } = useAuth();
+  const paths = useAdminRoutes();
+
+  if (!loggedIn) return <Navigate to={paths.home()} replace />;
+
+  return <Outlet />;
+};
 
 const App = () => {
   const paths = useAdminRoutes();
@@ -12,7 +32,9 @@ const App = () => {
       <Routes>
         <Route path={paths.home()} element={<Dashboard />}>
           <Route index element={<HomePage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="dashboard" element={<DashboardPage />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
